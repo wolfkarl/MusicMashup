c7ae8f2f97ebd018defdf5c1454df09bf6a6ed09
mr-flannery <dennis_hempfing@gmx.de>
2014-12-14 13:32:32 +0100

    Added the parser to the master branch.

--- MusicMashupArtist.py
+++ MusicMashupArtist.py
@@ -208,6 +208,7 @@ class MusicMashupArtist:
 		self._pull_producer_relation()
 		self._pull_current_bands_of_current_members()
 		self._pull_former_bands_of_current_members()
+		self.parse_to_rdf()
 		return self.related
 
 
@@ -312,6 +313,45 @@ class MusicMashupArtist:
 					self.relatedSources.append(result["band"]["value"])
 					self.related.append(MusicMashupArtist(self._uri_to_name(result["band"]["value"]), "Because "+self._uri_to_name(member)+" is also a member of this band."))
 					print (result["band"]["value"])
+
+
+	# ========================================================================================
+	# Parse Methoden
+	# ========================================================================================
+
+	def parse_to_rdf(self):
+		filename = self.get_name().lower().replace(' ', '_')
+		filepath = "dumps/"+filename
+		# fileExists = os.path.exists(filepath)
+		
+		file = open(filepath, 'w+')
+		
+		self.parse_prefixes(file)
+		self.parse_abstract(file)
+		self.parse_current_members(file)
+		
+		self.parse_related_artists(file)
+		file.close()
+	
+	def parse_prefixes(self, file):
+		file.write("@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n")
+		file.write("@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n")
+		file.write("@prefix mo: <http://purl.org/ontology/mo/> .\n")
+		file.write("@prefix dbpedia-owl: <http://dbpedia.org/ontology/> .\n")
+		file.write("@prefix dbprop: <http://dbpedia.org/property/> .\n")
+		file.write("@prefix owl: <http://www.w3.org/2002/07/owl#> .\n\n")
+	
+	def parse_abstract(self, file):
+		file.write("<"+self.dbpediaURL+"> dbpedia-owl:abstract \""+self.abstract+"\" .\n")
+	
+	def parse_current_members(self, file):
+		for member in self.currentMembers:
+			file.write("<"+self.dbpediaURL+"> dbprop:currentMember <"+member+"> .\n")
+	def parse_related_artists(self, file):
+		for artist in self.relatedSources:
+			file.write("<"+self.dbpediaURL+"> dbpedia-owl:associatedMusicalArtist <"+artist+"> .\n")
+
+
 
 # run from console for test setup
 if __name__ == '__main__':
