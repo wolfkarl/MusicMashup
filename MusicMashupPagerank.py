6094fc43fc94c8980b14c6a46db7db987aeddc8d
paul wille <paul.wille@t-online.de>
2015-02-02 15:01:18 +0100

    implemented pagerank in voting and MusicmashupArtistClass, ref #10

--- MusicMashupPagerank.py
+++ MusicMashupPagerank.py
@@ -15,25 +15,29 @@ class MusicMashupPagerank(object):
 		print ("[*] Finished parsing")
 
 	def get_pagerank(self, resource):
-		print ("[~] Querying Pagerank for: "+resource)
 		pagerank = None
-		try:
-			results = self.mygraph.query("""
-				PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
-				SELECT DISTINCT ?rank WHERE {
-				<"""+resource+"""> dbpedia-owl:wikiPageRank ?rank
+		if resource != -1:
+			print ("[~] Querying Pagerank for: "+resource)
+			try:
+				results = self.mygraph.query("""
+					PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+					SELECT DISTINCT ?rank WHERE {
+					<"""+resource+"""> dbpedia-owl:wikiPageRank ?rank
 
-				}
-			""")
-			for row in results:
-				pagerank = row[0]
-		except:
-			pass
+					}
+				""")
+				for row in results:
+					pagerank = row[0]
+			except:
+				pass
 
 
 		if pagerank:
 			print "[+] Found Pagerank: ", pagerank
-			return pagerank
+			return float(pagerank)
 		else:
 			print ("[-] Did not find Pagerank, returning default Value")
 			return defaultValue
+
+# paggy = MusicMashupPagerank()
+# paggy.get_pagerank("http://dbpedia.org/resource/The_Bakerton_Group")
