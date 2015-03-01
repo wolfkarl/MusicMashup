c82a057feef4248779fd3abe8b1bb02d8bc3a3b6
Karl Wolf <wolfkarl@gmail.com>
2015-03-01 20:11:09 +0100

    Die Kommentare sind jetzt fast Thomas Mann

--- MusicMashupServer.py
+++ MusicMashupServer.py
@@ -1,17 +1,21 @@
-# server shizzle
+#### this is the server class as well as the starter script for MusicMashup.
+
+# server is build upon cherrypy
 import cherrypy
 import os
 
-# eigentliche arbeit macht die artist klasse
+# the actual program logic is handled by the artist class
 from MusicMashupArtist import MusicMashupArtist
 
-# template gedoens
+# template engine
 from mako.template import Template
 from mako.lookup import TemplateLookup
-from titlecase import titlecase
 
-# fuer history generation
+
+# used for history / breadcrumbs generation
 from urllib import quote_plus
+# to make band names pretty in breadcrumbs
+from titlecase import titlecase
 
 class MusicMashupServer(object):
 
@@ -19,8 +23,9 @@ class MusicMashupServer(object):
 		pass
 
 
-	@cherrypy.expose # wird von cherrypy auf eine URL gemappt
+	@cherrypy.expose # tell cherrypy to map this function on "/" URL
 	def index(self, query="", soloartist=0):
+
 		# initialize mako (template engine)
 		lookup = TemplateLookup(directories=['html'])
 		
@@ -30,18 +35,21 @@ class MusicMashupServer(object):
 			tmpl = lookup.get_template("search.htm")
 			return tmpl.render()
 
-		# query is present
+		# query is present. could be a dbpedia URL or text search, artist class will deal with that
 		else: 
-			# create musicmashup object based on query:
+
+			### create musicmashup object based on query. this handles everything but the breadcrumbs.
 			self.artist = MusicMashupArtist(query)
 
-			# add new query to breadcrumbs list. create as list if not present
+			### save things to breadcrumbs and prepare them to be shown on the page
+
+			# add new query to breadcrumbs list. create as list if not existing yet
 			if not "history" in cherrypy.session:
 				cherrypy.session['history'] = []
 
 			# new search -> new breadcrumbs
-			if not query[:4] == "http":
-				cherrypy.session['history'] = []
+			if not query[:4] == "http": # if it's not a URL, it must be text search
+				cherrypy.session['history'] = [] # reset history list
 
 				# also, if name rather than query, convert to titlecase
 				query = titlecase(query)
@@ -50,16 +58,12 @@ class MusicMashupServer(object):
 			if not (len(cherrypy.session['history']) > 0 and cherrypy.session['history'][-1] == query):
 				cherrypy.session['history'].append(query)
 
-			# make sure the list has no more than 5 entries
-			maxentries = 10
-			if len(cherrypy.session['history']) > maxentries:
-				cherrypy.session['history'].pop
-
-
 			# load mako templates
 			tmpl = lookup.get_template("main.htm")
 
 			# add whole Artist object and history array from sessions
+			# all queries etc. will be triggered from within the template.
+			# basically, all information is fetched "on demand" by the artist object
 			return tmpl.render(artist=self.artist, history=cherrypy.session['history'])		
 
  
@@ -67,8 +71,14 @@ class MusicMashupServer(object):
 
 # End of class
 
+#### Startup script
+
 if __name__ == '__main__':
 	print ("[~] Initializing...")
+
+	# configure cherrypy entity, especially turn on session handling and define ./static 
+	# as the folder to serve images, css etc.
+
 	# bind to all IPv4 interfaces
 	cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 	conf = {
@@ -82,4 +92,5 @@ if __name__ == '__main__':
         }
     }
 
+    # start the server based on the functions of the MM-Server class
 	cherrypy.quickstart(MusicMashupServer(), '/', conf)
